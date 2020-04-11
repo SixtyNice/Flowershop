3dcb0be8164930120e17916859a9e6a81e323469
Dima Skibin <sixtynice77@gmail.com>
2020-04-11 10:35:39 +0300

    Make flower controller with method to get all flowers, get flower by name, get flowers by price range

---
+++ src/main/java/com/accenture/flowershop/Controller/FlowerController.java
@@ -0,0 +1,35 @@
+package com.accenture.flowershop.Controller;
+
+
+import com.accenture.flowershop.Model.FlowerService.FlowerService;
+import com.accenture.flowershop.entity.FlowerEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
+@RequestMapping("/flowers")
+public class FlowerController {
+
+    @Autowired
+    FlowerService flowerService;
+
+    @GetMapping(path = "/list", produces = "application/json")
+    public List<FlowerEntity> getAllFlowers() {
+        return flowerService.getAllFlowers();
+    }
+
+    @GetMapping(path = "/getflowerbyname/{name}", produces = "application/json")
+    public FlowerEntity getFlowerByName(@PathParam("name") String name) {
+        return flowerService.getFlowerByName(name);
+    }
+
+    @GetMapping(path = "/getflowerbyprice", produces = "application/json")
+    public List<FlowerEntity> getFlowerByPrice(@RequestParam("pricefrom") Integer priceFrom, @RequestParam("priceto") Integer priceTo) {
+        return flowerService.getFlowersByPrice(priceFrom, priceTo);
+
+    }
+
+}
