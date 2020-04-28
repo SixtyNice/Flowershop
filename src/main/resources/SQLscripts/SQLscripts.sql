57d52f8b7a35a6f87ad6f6e5d2096e266f61939f
Dima Skibin <sixtynice77@gmail.com>
2020-04-28 13:20:59 +0300

    Sql scripts for BD

--- src/main/resources/SQLscripts/SQLscripts.sql
+++ src/main/resources/SQLscripts/SQLscripts.sql
@@ -1,34 +1,57 @@
 --create table users
 
-      create TABLE "USERS" (
-      	"ID" UUID PRIMARY KEY,
-       	"LOGIN" VARCHAR2(45 CHAR) NOT NULL,
-        "PASSWORD" VARCHAR2(45 CHAR) NOT NULL,
-        "NAME" VARCHAR2(45 CHAR) NOT NULL,
-        "LAST_NAME" VARCHAR2(45 CHAR) NOT NULL,
-        "MIDDLE_NAME" VARCHAR2(45 CHAR),
-        "ADDRESS" VARCHAR(45 CHAR) NOT NULL,
-        "BALANCE" INT NOT NULL,
-        "DISCOUNT" INT NOT NULL,
-        );
+create TABLE "USERS"(
+"ID" UUID PRIMARY KEY,
+"LOGIN" VARCHAR2(45 CHAR)NOT NULL,
+"PASSWORD" VARCHAR2(45 CHAR)NOT NULL,
+"NAME" VARCHAR2(45 CHAR)NOT NULL,
+"LAST_NAME" VARCHAR2(45 CHAR)NOT NULL,
+"MIDDLE_NAME" VARCHAR2(45 CHAR),
+"ADDRESS" VARCHAR(45 CHAR)NOT NULL,
+"BALANCE" INT NOT NULL,
+"DISCOUNT" INT NOT NULL,
+);
 
-      create TABLE "FLOWERS" (
-      	"NAME" VARCHAR2(45 CHAR) PRIMARY KEY,
-      	"PRICE" INT NOT NULL,
-        "AMOUNT" INT NOT NULL,
-        );
+create TABLE "FLOWERS"(
+"ID" long not null,
+"NAME" VARCHAR2(45 CHAR)PRIMARY KEY,
+"PRICE" INT NOT NULL,
+"AMOUNT" INT NOT NULL,
+);
 
- create TABLE "CART" (
-      	"NAME" VARCHAR2(45 CHAR) PRIMARY KEY,
-      	"PRICE" INT NOT NULL,
-        "AMOUNT" INT NOT NULL,
-        );
+
+--create CART table
+
+create TABLE "CART"(
+"ID" INT PRIMARY KEY NOT NULL,
+"USER_ID" UUID NOT NULL,
+ FOREIGN KEY (USER_ID) REFERENCES USERS (ID),
+"FLOWER_NAME" VARCHAR2(45 CHAR) NOT NULL,
+"AMOUNT" INT NOT NULL
+);
+
+
 
 --create my admin
-insert into USERS (ID, LOGIN,PASSWORD,NAME,LAST_NAME,MIDDLE_NAME,ADDRESS,BALANCE,DISCOUNT) values ('','admin','admin123','Dima','Skibin','Norman','NO',0,0);
+insert into USERS(ID, LOGIN, PASSWORD, NAME, LAST_NAME, MIDDLE_NAME, ADDRESS, BALANCE, DISCOUNT)
+values (00000000-0000-0000-0000-000000000000, 'admin', 'admin123', 'Dima', 'Skibin', 'Norman', 'NO', 0, 0);
+--create user
+insert into USERS(ID, LOGIN, PASSWORD, NAME, LAST_NAME, MIDDLE_NAME, ADDRESS, BALANCE, DISCOUNT)
+values (00000000-0000-0000-0000-000000000001, 'dima', 'dima', 'dima', 'dima', 'Norman', 'NO', 10000, 10);
+
+----create my admin
+--insert into USERS(ID, LOGIN, PASSWORD, NAME, LAST_NAME, MIDDLE_NAME, ADDRESS, BALANCE, DISCOUNT)
+--values (00000000-0000-0000-0000-000000000000, 'admin', 'admin123', 'Dima', 'Skibin', 'Norman', 'NO', 0, 0);
+--insert into USERS(ID, LOGIN, PASSWORD, NAME, LAST_NAME, MIDDLE_NAME, ADDRESS, BALANCE, DISCOUNT)
+--values (00000000-0000-0000-0000-000000000001, 'dima', 'dima', 'dima', 'dima', 'Norman', 'NO', 10000, 100);
+--
 
 --create flowers
-insert into FLOWERS (NAME,PRICE,AMOUNT) values ('rose',60,100);
-insert into FLOWERS (NAME,PRICE,AMOUNT) values ('verbena',74,176);
-insert into FLOWERS (NAME,PRICE,AMOUNT) values ('begonia',80,150);
-insert into FLOWERS (NAME,PRICE,AMOUNT) values ('herbera',70,193);
+insert into FLOWERS(id, NAME, PRICE, AMOUNT)
+values (1, 'rose', 60, 100);
+insert into FLOWERS(id, NAME, PRICE, AMOUNT)
+values (2, 'verbena', 74, 176);
+insert into FLOWERS(id, NAME, PRICE, AMOUNT)
+values (3, 'begonia', 80, 150);
+insert into FLOWERS(id, NAME, PRICE, AMOUNT)
+values (4, 'herbera', 70, 193);
